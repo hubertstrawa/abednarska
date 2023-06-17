b8f01a53bb2ecc602f0897f66a693565a3f820ef
hubertstrawa <hubertstrawa@gmail.com>
2023-06-17 14:07:38 +0200

    init commit

--- theme.config.js
+++ theme.config.js
@@ -1,15 +1,34 @@
+import { BiLogoInstagramAlt } from 'react-icons/bi'
 const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <footer>
+    <footer
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
       <small>
-        <time>{YEAR}</time> © Your Name.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Aleksandra Bednarska.
+      </small>
+      <small>
+        <a
+          style={{ display: 'flex', alignItems: 'center', height: '20px' }}
+          target='_blank'
+          href='https://www.instagram.com/abednarska3/'
+        >
+          <img
+            style={{ width: '15px', marginRight: '5px' }}
+            src='/images/ig-logo.svg'
+          />{' '}
+          instagram
+        </a>
       </small>
       <style jsx>{`
         footer {
-          margin-top: 8rem;
+          margin-top: 0;
         }
         a {
           float: right;
